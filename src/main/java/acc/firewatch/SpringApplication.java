4e725a5c654fd08080e34b3c39a4c9c21e6c80d7
happine2s <happine2s@sookmyung.ac.kr>
2025-08-02 17:55:07 +0900

    Fix: secret manager 이름 수정

--- src/main/java/acc/firewatch/SpringApplication.java
+++ src/main/java/acc/firewatch/SpringApplication.java
@@ -1,7 +1,5 @@
 package acc.firewatch;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -10,13 +8,4 @@ public class SpringApplication {
 	public static void main(String[] args) {
 		org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
 	}
-
-	@Value("${jwt.secret.expiration.access-token}")
-	private long accessExpiration;
-
-	@PostConstruct
-	public void test() {
-		System.out.println(">>> " + accessExpiration);
-	}
-
 }
