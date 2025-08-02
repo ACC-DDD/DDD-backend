9a87dbe1354da5a8e27e6c3300d407a520916102
happine2s <happine2s@sookmyung.ac.kr>
2025-08-02 16:53:42 +0900

    Test: secret manager로부터 값을 받아오는지 확인

--- src/main/java/acc/firewatch/SpringApplication.java
+++ src/main/java/acc/firewatch/SpringApplication.java
@@ -1,5 +1,7 @@
 package acc.firewatch;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -7,6 +9,14 @@ public class SpringApplication {
 
 	public static void main(String[] args) {
 		org.springframework.boot.SpringApplication.run(SpringApplication.class, args);
+	}
+
+	@Value("${jwt.secret.expiration.access-token}")
+	private long accessExpiration;
+
+	@PostConstruct
+	public void test() {
+		System.out.println(">>> " + accessExpiration);
 	}
 
 }
